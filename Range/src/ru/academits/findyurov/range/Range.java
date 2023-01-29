6903a32a634c788cacaf65e65cae23da8094493f
Никита <nik.findyurov@mail.ru>
2023-01-29 16:39:27 +0700

    new commit

--- Range/src/ru/academits/findyurov/range/Range.java
+++ Range/src/ru/academits/findyurov/range/Range.java
@@ -33,6 +33,27 @@ public class Range {
         return number >= from && number <= to;
     }
 
+    @Override
+    public String toString() {
+        return "(" + from + ", " + to + ")";
+    }
+
+    public void print() {
+        System.out.print("(" + from + "," + to + ")" + ", ");
+    }
+
+    public static void arraysPrint(Range[] array) {
+        if (array.length == 0) {
+            System.out.print("[]");
+        } else {
+            System.out.print("[");
+            for (Range element : array) {
+                element.print();
+            }
+            System.out.print("]");
+        }
+    }
+
     public Range getIntersections(Range range1, Range range2) {
         if ((range1.to <= range2.from) || (range2.to <= range1.from)) {
             return null;
@@ -43,5 +64,33 @@ public class Range {
         return new Range(from, to);
     }
 
+    public Range[] getUnion(Range range1, Range range2) {
+        if ((range1.to < range2.from) || (range2.to < range1.from)) {
+            return new Range[]{new Range(range1.from, range1.to), new Range(range2.from, range2.to)};
+        }
 
+        double from = Math.min(range1.from, range2.from);
+        double to = Math.max(range1.to, range2.to);
+        return new Range[]{new Range(from, to)};
+    }
+
+    public Range[] getDifference(Range range1, Range range2) {
+        if ((range1.to <= range2.from) || (range2.to <= range1.from)) {
+            return new Range[]{new Range(range1.from, range1.to)};
+        }
+
+        if (range1.from >= range2.from && range1.to <= range2.to) {
+            return new Range[0];
+        }
+
+        if (range2.from > range1.from && range2.to < range1.to) {
+            return new Range[]{new Range(range1.from, range2.from), new Range(range2.to, range1.to)};
+        }
+
+        if (range2.from > range1.from) {
+            return new Range[]{new Range(range1.from, range2.from)};
+        }
+
+        return new Range[]{new Range(range2.to, range1.to)};
+    }
 }
--- Range/src/ru/academits/findyurov/range_main/Main.java
+++ Range/src/ru/academits/findyurov/range_main/Main.java
@@ -7,30 +7,53 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Range range = new Range(15, 30);
 
-        System.out.println("Getter implementation.");
-        System.out.println(range.getFrom());                         // реализация Геттера
-        System.out.println("Implementation of the setter.");
-        range.setFrom(19.5);                                          // реализация Сеттера
-        System.out.println("New value after the setter.");
-        System.out.println(range.getFrom());
-
-        System.out.println("Getter implementation.");
-        System.out.println(range.getTo());
-        System.out.println("Implementation of the setter.");
-        range.setTo(80.78);
-        System.out.println("New value after the setter.");
-        System.out.println(range.getTo());
+        Range range1 = new Range(35, 50);
+        Range range2 = new Range(20, 40);
 
         System.out.println("The length of the range from the initial number to the extreme.");
-        System.out.print(range.getLength());           // метод для вычисления длины диапазона
+        System.out.print(range1.getLength());           // метод для вычисления длины диапазона
 
         System.out.println();
 
-        System.out.println("Enter number between first number to last number:");
-        double number = scanner.nextDouble();
+        System.out.println("range intersections");
+        System.out.println(range1.toString());
+        System.out.print("and ");
+        System.out.println(range2.toString());
 
-        System.out.println(range.isInside(number));    // применение метода isInside
+        Range intersection;
+
+        intersection = range1.getIntersections(range1, range2);
+
+        if (intersection != null) {
+            System.out.println(intersection.toString());
+        } else {
+            System.out.println("no intersections");
+        }
+
+        System.out.println();
+
+        System.out.print("Unification range: ");
+        System.out.println(range1.toString());
+        System.out.print("and ");
+        System.out.println(range2.toString());
+        Range[] union = range1.getUnion(range1, range2);
+
+        Range.arraysPrint(union);
+
+        System.out.println();
+
+        System.out.print("Range difference: ");
+        System.out.println(range1.toString());
+        System.out.print("and ");
+        System.out.print(range2.toString());
+        System.out.println();
+        Range[] difference = range1.getDifference(range1, range2);
+
+        if (difference.length != 0) {
+            Range.arraysPrint(difference);
+        } else {
+            System.out.print("No difference");
+        }
     }
 }
