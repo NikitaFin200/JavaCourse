19172a1d8f7c6a74297dbaa65c3c8ab177ab89b9
Nikita <nik.findyruv@mail.ru>
2023-02-02 21:27:14 +0700

    new commit

---
+++ Vector/src/ru/academits/findyurov/Main/Main.java
@@ -0,0 +1,61 @@
+package ru.academits.findyurov.Main;
+
+import ru.academits.findyurov.Vector;
+
+import static ru.academits.findyurov.Vector.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Vector vector1 = new Vector(2);
+        vector1.setCoordinate(0, 1);
+        vector1.setCoordinate(1, 0);
+        System.out.println("vector 1 = " + vector1.toString());
+        System.out.println("Its dimension = " + vector1.getSize());
+        System.out.println("Its length = " + vector1.length());
+        int i = 1;
+        System.out.println("His " + (i + 1) + "coordinates = " + vector1.getCoordinate(i));
+        System.out.println();
+
+        Vector vector2 = new Vector(vector1);
+        System.out.println("vector 2 (copy vector 1) = " + vector2.toString());
+        System.out.println("Its dimension = " + vector2.getSize());
+        System.out.println("Its length = " + vector2.length());
+        System.out.println();
+
+        double[] support = {1, 2, 3};
+        Vector vector3 = new Vector(support);
+        System.out.println("Its dimension = " + vector3.getSize());
+        System.out.println("Its length = " + vector3.length());
+        vector3.subVector(vector1);
+        System.out.println("vector 3 = vector 3 - vector 1 = " + vector3.toString());
+        System.out.println("vector 1 = " + vector1.toString());
+        System.out.println();
+
+        Vector vector4 = new Vector(4, support);
+        System.out.println("Its dimension = " + vector4.getSize());
+        System.out.println("Its length = " + vector4.length());
+        vector4.addVector(vector1);
+        System.out.println("vector 4 = vector 4 + vector 1 = " + vector4.toString());
+        vector1.addVector(vector4);
+        System.out.println("vector 1 = vector 1 + new vector 4 = " + vector1.toString());
+        System.out.println();
+
+        int alpha = 6;
+        vector1.scalarMul(6);
+        System.out.println("vector 1 * " + alpha + " = " + vector1.toString());
+        vector1.turnVector();
+        System.out.println("vector 1 = expanded vector vector1 * " + alpha + " this " + vector1.toString());
+        System.out.println();
+
+        System.out.println("static methods");
+        Vector vector5 = new Vector(addVector(vector1, vector3));
+        System.out.println("vector 5 = vector 1 + vector 3 = " + vector5.toString());
+
+        Vector vector6 = new Vector(subVector(vector1, vector3));
+        System.out.println("vector 6 = vector 1 - vector 3 = " + vector6.toString());
+
+        vector6.addVector(new Vector(new double[]{0, 0, 0, 0, 0}));
+        System.out.println("vector 6 = " + vector6.toString());
+        System.out.println("vector 5 * vector 6 = " + scalarMul(vector5, vector6));
+    }
+}
---
+++ Vector/src/ru/academits/findyurov/Vector.java
@@ -0,0 +1,164 @@
+package ru.academits.findyurov;
+
+import java.util.Arrays;
+
+public class Vector {
+    public double[] coordinates;
+
+    public Vector(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Illegal Argument Exception");
+        }
+
+        coordinates = new double[n];
+    }
+
+    public Vector(Vector vector) {
+        int n = vector.getSize();
+        coordinates = Arrays.copyOf(vector.coordinates, n);
+    }
+
+    public Vector(double[] coordinates) {
+        int n = coordinates.length;
+
+        if (n == 0) {
+            throw new IllegalArgumentException("Illegal Argument Exception");
+        }
+
+        this.coordinates = Arrays.copyOf(coordinates, n);
+    }
+
+    public Vector(int n, double[] coordinates) {
+        if (n <= 0 || coordinates.length == 0) {
+            throw new IllegalArgumentException("Illegal Argument Exception");
+        }
+
+        this.coordinates = Arrays.copyOf(coordinates, n);
+    }
+
+    public int getSize() {
+        return coordinates.length;
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("{");
+        int size = getSize();
+
+        for (int i = 0; i < size - 1; i++) {
+            stringBuilder.append(coordinates[i]).append(", ");
+        }
+
+        stringBuilder.append(coordinates[size - 1]).append("}");
+        return stringBuilder.toString();
+    }
+
+    public void addVector(Vector vector) {
+        int size = this.getSize();
+        int vectorSize = vector.getSize();
+
+        if (size < vectorSize) {
+            this.coordinates = Arrays.copyOf(coordinates, vectorSize);
+        }
+
+        for (int i = 0; i < vectorSize; ++i) {
+            this.coordinates[i] += vector.coordinates[i];
+        }
+    }
+
+    public void subVector(Vector vector) {
+        int size = this.getSize();
+        int vectorSize = vector.getSize();
+
+        if (size < vectorSize) {
+            this.coordinates = Arrays.copyOf(coordinates, vectorSize);
+        }
+
+        for (int i = 0; i < vectorSize; ++i) {
+            this.coordinates[i] -= vector.coordinates[i];
+        }
+    }
+
+    public void scalarMul(double number) {
+        int size = coordinates.length;
+
+        for (int i = 0; i < size; i++) {
+            this.coordinates[i] *= number;
+        }
+    }
+
+    public void turnVector() {
+        this.scalarMul(-1.0);
+    }
+
+    public double length() {
+        double support = 0;
+
+        for (double elem : this.coordinates) {
+            support += Math.pow(elem, 2);
+        }
+
+        return Math.sqrt(support);
+    }
+
+    public double getCoordinate(int index) {
+        return coordinates[index];
+    }
+
+    public void setCoordinate(int index, double elem) {
+        coordinates[index] = elem;
+    }
+
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Vector p = (Vector) obj;
+
+        if (this.getSize() != p.getSize()) {
+            return false;
+        }
+
+        int size = coordinates.length;
+
+        for (int i = 0; i < size; i++) {
+            if (this.getCoordinate(i) != p.getCoordinate(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int hashCode() {
+        return Arrays.hashCode(coordinates);
+    }
+
+    public static Vector addVector(Vector vector1, Vector vector2) {
+        Vector vector = new Vector(vector1);
+        vector.addVector(vector2);
+        return vector;
+    }
+
+    public static Vector subVector(Vector vector1, Vector vector2) {
+        Vector vector = new Vector(vector1);
+        vector.subVector(vector2);
+        return vector;
+    }
+
+    public static double scalarMul(Vector vector1, Vector vector2) {
+        double mul = 0;
+        int size = Math.min(vector1.getSize(), vector2.getSize());
+
+        for (int i = 0; i < size; ++i) {
+            mul += vector1.coordinates[i] * vector2.coordinates[i];
+        }
+
+        return mul;
+    }
+}
