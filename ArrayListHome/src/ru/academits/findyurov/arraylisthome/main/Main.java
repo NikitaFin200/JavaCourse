595c70167b277f74e21abddc308a3ede139ededf
nikfi <nikitafindyurov@yandex.ru>
2023-03-09 00:57:19 +0700

    complete

--- ArrayListHome/src/ru/academits/findyurov/arraylisthome/main/Main.java
+++ ArrayListHome/src/ru/academits/findyurov/arraylisthome/main/Main.java
@@ -1,45 +1,88 @@
 package ru.academits.findyurov.arraylisthome.main;
 
-import ru.academits.findyurov.arraylisthome.list.ArrayListHome;
-
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Locale;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
+    private static final ArrayList<Object> list = new ArrayList<>();
 
-        ArrayListHome obj = new ArrayListHome();
-        ArrayList<String> list = new ArrayList<>(obj.read());
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>(Main.getStringsFromFile("file.txt"));
         System.out.println("List from file");
         System.out.println(list);
 
         System.out.println("The list is cleared");
-        obj.clear();
+        list.clear();
+        System.out.println();
 
-        int number;
+        list.add(10);
+        list.add(11);
+        list.add(20);
+        list.add(30);
+        list.add(33);
+        list.add(53);
+        list.add(100);
+        list.add(101);
+        list.add(102);
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Enter number");
-            number = scanner.nextInt();
-            obj.addNumber(number);
-        }
         System.out.println("The list that has just been filled in");
-        System.out.println(obj.getList());
+        System.out.println(list);
+        System.out.println();
 
-        obj.deleteEvenNumbers();
+        Main.deleteEvenNumbers(list);
         System.out.println("List without even numbers");
-        System.out.println(obj.getList());
+        System.out.println(list);
+        System.out.println();
 
-        ArrayList<String> list3 = new ArrayList<>(Arrays.asList("1", "2", "3", "2", "10", "14", "10"));
-        obj.getCopy(list3);
+        ArrayList<Object> list3 = new ArrayList<>(Arrays.asList(1, 2, 3, 2, 10, 14, 10));
+        list.clear();
+        list.addAll(list3);
         System.out.println("List with integer numbers, that are repeated");
-        System.out.println(obj.getList());
-        obj.deleteRepeat();
+        System.out.println(list);
+        System.out.println();
+
         System.out.println("List without repeat numbers");
-        System.out.println(obj.getList());
+        System.out.println(Main.deleteRepeat(list));
+        System.out.println();
+    }
+
+    public static ArrayList<Object> getStringsFromFile(String str) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(str));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                list.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Could not find the file");
+        }
+
+        return list;
+    }
+
+    public static void deleteEvenNumbers(ArrayList<Object> list) {
+        list.removeIf(object -> (Integer) object % 2 == 0);
+    }
+
+    public static ArrayList<Object> deleteRepeat(ArrayList<Object> list) {
+        ArrayList<Object> newList = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+           boolean repeat = false;
+
+            for (int j = i + 1; j < list.size(); j++) {
+                if (Arrays.equals(new Object[]{list.get(j)}, new Object[]{list.get(i)})) {
+                    repeat = true;
+                    break;
+                }
+            }
+
+            if (!(repeat)) {
+                newList.add(list.get(i));
+           }
+        }
+
+        return newList;
     }
 }
