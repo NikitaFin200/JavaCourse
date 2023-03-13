648d571b47f76cf4143aad7245b2c79c1ec8ccab
nikfi <nikitafindyurov@yandex.ru>
2023-03-13 23:57:14 +0700

    new commit

--- Matrix/src/ru/academits/findyurov/matrix/main/Main.java
+++ Matrix/src/ru/academits/findyurov/matrix/main/Main.java
@@ -23,7 +23,7 @@ public class Main {
         System.out.println(matrix2.toString());
         System.out.println();
 
-        System.out.println("determinantttttt = " + matrix2.getDeterminant());
+        System.out.println("determinant = " + matrix2.getDeterminant());
         System.out.println("Columns quantity = " + matrix2.getColumnsCount());
         System.out.println("Rows quantity = " + matrix2.getRowsCount());
         System.out.println();
