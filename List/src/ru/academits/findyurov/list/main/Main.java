93b2163f21305599ebda754a03ee51feadff6279
Nikita <nik.findyruv@mail.ru>
2023-04-03 19:39:20 +0700

    new commit

---
+++ List/src/ru/academits/findyurov/list/main/Main.java
@@ -0,0 +1,19 @@
+package ru.academits.findyurov.list.main;
+
+//import java.util.LinkedList;
+
+
+import ru.academits.findyurov.list.list.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(10);
+        list.add(20);
+
+        list.print();
+
+
+    }
+}
