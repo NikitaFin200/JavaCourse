3a03851a87cd5ce2bb9d657c3b3b2037511c4529
Nikita <nik.findyruv@mail.ru>
2023-04-06 00:13:23 +0700

    35%

--- List/src/ru/academits/findyurov/list/list/LinkedList.java
+++ List/src/ru/academits/findyurov/list/list/LinkedList.java
@@ -1,64 +1,127 @@
 package ru.academits.findyurov.list.list;
 
-public class LinkedList {
-    private Node head;
+import ru.academits.findyurov.list.node.ListNode;
+
+public class LinkedList<T> {
+    private ListNode<T> head;
+    private int size;
 
     public LinkedList() {
         head = null;
+        size = 0;
     }
 
-    public class Node {
-        public int data;
-        public Node next;
+    public int getSize() {
+        return size;
+    }
 
-        public Node(int data) {
-            this.data = data;
-            next = null;
+    public T getFirstValue() {
+        if (head == null) {
+            return null;
         }
+
+        return head.getValue();
     }
 
-    public void add(int data) {
-        Node newNode = new Node(data);
-        Node currentNode = head;
+    public T getValue(int index) {
+        ListNode<T> currentNode = head;
+        int currentIndex = 0;
+
+        while (currentNode != null) {
+            if (currentIndex == index) {
+                return currentNode.getValue();
+            }
+
+            currentNode = currentNode.getNext();
+            currentIndex++;
+        }
+
+        return null;
+    }
+
+    public T setValue(int index, T value) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
+        }
+
+        ListNode<T> currentNode = head;
+        int currentIndex = 0;
+
+        while (currentNode != null) {
+            if (currentIndex == index) {
+                System.out.println("Old element:" + currentNode.getValue());
+                currentNode.setValue(value);
+                System.out.println("New element:" + currentNode.getValue());
+                return currentNode.getValue();
+            }
+
+            currentNode = currentNode.getNext();
+            currentIndex++;
+        }
+
+        return null;
+    }
+
+    public void addValue(T data) {
+        ListNode<T> newNode = new ListNode<>(data);
+        ListNode<T> currentNode = head;
 
         if (head == null) {
             head = newNode;
+            size++;
         } else {
-            while (currentNode.next != null) {
-                currentNode = currentNode.next;
+            while (currentNode.getNext() != null) {
+                currentNode = currentNode.getNext();
+                size++;
             }
 
-            currentNode.next = newNode;
-        }
-    }
-
-    public void remove(int data) {
-        Node currentNode = head;
-        Node previousNode = null;
-
-        while (currentNode.next != null) {
-            if (currentNode.data == data) {
-                if (currentNode == head) {
-                    head = currentNode.next;
-                } else {
-                    previousNode.next = currentNode.next;
-                }
-            }
-            previousNode = currentNode;
-            currentNode = currentNode.next;
+            currentNode.setNext(newNode);
         }
     }
 
     public void print() {
-        Node currentNode = head;
+        ListNode<T> currentNode = head;
 
         if (head != null) {
-            System.out.println(head.data);
+            System.out.println(head.getValue());
         }
 
-        while (currentNode.next != null) {
-            currentNode = currentNode.next;
-            System.out.println(currentNode.data);
+        if (currentNode != null) {
+            while (currentNode.getNext() != null) {
+                currentNode = currentNode.getNext();
+                System.out.println(currentNode.getValue());
+            }
         }
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
+        }
+
+        ListNode<T> currentNode = head;
+        ListNode<T> previousNode = null;
+        int currentIndex = 0;
+
+        while (currentNode.getNext() != null) {
+            if (currentIndex == index) {
+                if (currentIndex == 0) {
+                    head = currentNode.getNext();
+                } else {
+                    previousNode.setNext(currentNode.getNext());
+                }
+            }
+
+            previousNode = currentNode;
+            currentNode = currentNode.getNext();
+            currentIndex++;
+        }
+    }
+
+    public void enterElementFirst(T value) {
+        ListNode<T> node = new ListNode<>(value);
+        node.setNext(head);
+        head = node;
+        size++;
     }
 }
--- List/src/ru/academits/findyurov/list/main/Main.java
+++ List/src/ru/academits/findyurov/list/main/Main.java
@@ -1,19 +1,39 @@
 package ru.academits.findyurov.list.main;
 
-//import java.util.LinkedList;
-
-
 import ru.academits.findyurov.list.list.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
+        LinkedList<java.io.Serializable> lis = new LinkedList<>();
 
-        list.add(10);
-        list.add(20);
+        lis.addValue(2);
+        lis.addValue(10);
+        lis.addValue(11);
+        lis.addValue("rrr");
 
-        list.print();
+        System.out.print("Print list");
+        lis.print();
+        System.out.println();
 
+        System.out.print("Get list size:");
+        System.out.println(lis.getSize());
 
+        System.out.print("Get first element:");
+        System.out.println(lis.getFirstValue());
+
+        System.out.print("Get element for index:");
+        System.out.println(lis.getValue(2));
+
+        System.out.println("Zamena");
+        System.out.println(lis.setValue(1, 100));
+
+        System.out.println("remove");
+        lis.remove(1);
+        lis.print();
+        System.out.println();
+
+        System.out.println("first");
+        lis.enterElementFirst("rr");
+        lis.print();
     }
 }
